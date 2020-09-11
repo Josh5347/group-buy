89cfc39b36913cbc1cc3ba5a3c1a1f34ee8c2f80
Josh Chang <joshcc510@yahoo.com.tw>
2020-09-11 18:22:01 +0800

    新增訂單輸入資料驗證。新增訂單輸入資料處理，符合資料庫格式。

--- addOrder.php
+++ addOrder.php
@@ -46,6 +46,64 @@
     }
   }
 
+  function createOrder(){
+
+    $arrayPrice = [];
+
+    $i = 1;
+    do{
+      array_push($arrayPrice, $_REQUEST['price'.$i]);
+      $i++;
+    }while (isset($_REQUEST['price'.$i]));
+
+
+    //驗證通過
+    if(validation($arrayPrice )){
+
+      foreach($_REQUEST['product'] as $product){
+
+      }
+    }
+  }
+
+  function validation($arrayPrice){
+    global $errors;
+
+    if(count($_REQUEST['product']) != count($arrayPrice)){
+      $errors = "有數量欄未輸入";
+      return false;
+    }else{
+      return true;
+    }
+  }
+
+  function insert_order(){
+
+    global $connOO;
+
+    $query = sprintf("INSERT INTO order 
+    (public_flag, store_name, Introduction, store_tel, store_address, detail, 
+    store_fax, store_url, create_username, create_date, update_date) 
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s )", 
+    0, 
+    GetSQLValue($_REQUEST['store_name'], "text"), 
+    GetSQLValue($_REQUEST['introduction'], "text"), 
+    GetSQLValue($_REQUEST['store_tel'], "text"), 
+    GetSQLValue($_REQUEST['store_address'], "text"), 
+    GetSQLValue($_REQUEST['detail'], "text"), 
+    GetSQLValue($_REQUEST['store_fax'], "text"), 
+    GetSQLValue($_REQUEST['store_url'], "text"), 
+    GetSQLValue($_SESSION['username'], "text"), 
+    GetSQLValue(date("Y-m-d H:i:s"), "date"), 
+    GetSQLValue(date("Y-m-d H:i:s"), "date"));
+
+  
+    // 傳回結果集
+    $result = mysqli_query($connOO, $query);
+    
+    return $result;
+  }
+
   /****************************************************/
   /*                    main                          */
   /****************************************************/
@@ -53,7 +111,7 @@
   //錯誤訊息
   $errors = [];
   $i = 0;
-  $rowBuyInfo = [];
+  $rowBuyInfo = ['store_name' => ''];
   $arrayProducts = [];
 
   if(isset($_GET['order_id'])){
@@ -61,7 +119,7 @@
   }
 
   if(isset($_POST['create_order'])){
-    var_dump($_POST);
+    createOrder();
   }
 
 ?>
@@ -96,6 +154,8 @@
             <h1 class="h3 mb-0 text-gray-800">新增訂單</h1>
           </div>
 
+          <!-- 錯誤訊息 -->
+          <?php require_once 'common/validationErrorMessage.php'?>
 
           <div class="row">
 
@@ -139,16 +199,22 @@
                         <tr>
                           <!-- 陣列中取出array[0]為產品名 -->
                           <td><?= $product = array_shift($rowOthers);?></td>
-                          <td>
+                          <td data-product-no="<?=$i;?>">
 <?php
+      $j = 0;
       foreach($rowOthers as $other){
         //比對如 " 中杯 40"，比對成功增加一個radio
         if(preg_match('/^(\s)*[(\x7f-\xff)a-zA-Z]+(\s)+[0-9]+/',$other)){
-          $arrayOther = preg_split('/[\s]+/', $other);
+          $j++;
+          $arrayOther = preg_split('/[\s]+/', $other);         
           $arrayPrice = preg_grep('/[0-9]+/', $arrayOther );
+          $arraySubProduct = preg_grep('/[(\x7f-\xff)a-zA-Z]+/', $arrayOther );
+          // 取得商品細項的價格
           $price = array_shift($arrayPrice);
-          printf("<input type='radio' name='price%d' class='subPrice' value=%d />&nbsp%s&nbsp&nbsp&nbsp",
-          $i, $price, $other );
+          // 取得商品細項的名稱
+          $subProduct = array_shift($arraySubProduct);
+          printf("<input type='radio' class='subPrice' name='price%s' value=%d data-sub-product='%s' data-sub-product-no='%s' />&nbsp%s&nbsp&nbsp&nbsp",
+          $i, $price, $subProduct, $j, $other );
         }else{
           echo $other;
           printf("<input type='hidden' class='price' value='%s'", $other);
@@ -158,13 +224,13 @@
                           </td>
                           <td class="td_amount form-inline" data-product-no="<?=$i;?>">
                             <input type="text" class="amount form-control" size="2" readonly />
-                            <button type="button" class="plus btn btn-secondary btn-sm ml-2" onclick="plus(<?=$i;?>)">+</button>
+                            <button type="button" class="plus btn btn-secondary btn-sm ml-2">+</button>
                             <button type="button" class="minus btn btn-secondary btn-sm ml-2">-</button>
                             <input type="hidden" class="product" value="<?= $product;?>" />
-                            
+                            <input type="hidden" class="product-no" value="<?= $i;?>" />
                           </td>
                           <td>
-                            <input type="text" class="form-control" />
+                            <input type="text" class="explain form-control"  />
                           </td>
                         </tr>
 <?php
@@ -173,10 +239,16 @@
 ?>
                       </tbody>                    
                     </table>
-                    <div>
-                        <button type="submit" name="create_order" class="btn btn-danger btn-user">確認訂購</button>
+                    <div class="row">
+                      <div class="form-group col-md-4">
+                        <input type="text" name="orderer" class="form-control form-control-user" 
+                        placeholder="訂購人:必輸" required />
+                      </div>
+                      <div class="col-md-8">
+                          <button type="submit" name="create_order" class="btn btn-danger btn-user mr-3">確認訂購</button>
+                          <button type="button" class="btn btn-outline-danger btn-user" onclick="location.href='/home.php'">取消訂購</button>
+                      </div>
                     </div>
-
                   </form>
                 </div>
               </div>
--- js/addOrder.js
+++ js/addOrder.js
@@ -1,24 +1,32 @@
 $(function () {
+  //原本radio按鈕name屬性值
   var radioAttrName;
 
   //按下數量按鈕"+"
   $('.td_amount button.plus').click(function(){
     var tagInputAmount = $(this).parent().find('.amount');
     var tagInputProduct = $(this).parent().find('.product');
+    var tagInputExpalin = $(this).parent().next().find('.explain');
     var tagRadioSubPrice = $(this).parent().prev().find('.subPrice');
     var tagInputPrice = $(this).parent().prev().find('.price');
+    var tagInputProductNo = $(this).parent().find('.product-no');
     var productNo = $(this).parent().data("product-no");
     var amount = tagInputAmount.val();
     if(amount == ''){
       amount = 1;
+      // 價錢欄不使用array
       var attrNamePrice = "price" + productNo;
       // 若數量欄有數字，將此欄name設成amount[]，即可透過表單傳送
       tagInputAmount.attr("name", "amount[]");
       tagInputProduct.attr("name", "product[]");
+      tagInputExpalin.attr("name", "explain[]");
       tagInputPrice.attr("name", attrNamePrice);
+      tagInputProductNo.attr("name", "product_no[]")
 
-      // radioAttrName = tagRadioSubPrice.attr("name");
-      //tagRadioSubPrice.attr("name", "price[]");
+      radioAttrName = tagRadioSubPrice.attr("name");
+      tagRadioSubPrice.attr("name", attrNamePrice);
+      //點擊價錢第一個radio
+      tagRadioSubPrice.first().click();
 
     }else{
       ++amount;
@@ -31,8 +39,10 @@ $(function () {
   $('.td_amount button.minus').click(function(){
     var tagInputAmount = $(this).parent().find('.amount');
     var tagInputProduct = $(this).parent().find('.product');
+    var tagInputExpalin = $(this).parent().next().find('.explain');
     var tagRadioSubPrice = $(this).parent().prev().find('.subPrice');
     var tagInputPrice = $(this).parent().prev().find('.price');
+    var tagInputProductNo = $(this).parent().find('.product-no');
     var amount = tagInputAmount.val();
     //當數量=空 或 0 時 按下-扭會將數量清成空白
     if(amount == '' || amount == 1){
@@ -40,9 +50,15 @@ $(function () {
       // 若數量欄無數字，將此欄name屬性移除，即不透過表單傳送
       tagInputAmount.removeAttr("name");
       tagInputProduct.removeAttr("name");  
+      tagInputExpalin.removeAttr("name");  
       tagInputPrice.removeAttr("name"); 
+      tagInputProductNo.removeAttr("name");
+      //取消點擊價錢radio
       tagRadioSubPrice.prop("checked", "");
-      // tagRadioSubPrice.removeAttr("name");
+      //將radio的屬性值還原
+      tagRadioSubPrice.attr("name", radioAttrName);
+      //tagRadioSubPrice.removeAttr("name");
+
          
     }else{
       --amount;
@@ -51,5 +67,21 @@ $(function () {
     tagInputAmount.val(amount);
   });
 
-  
+  // 按下價錢的radio按鈕，將radio商品名稱加在名稱之後 例:奶茶:S
+  $('.subPrice').click(function () {
+    var productNo = $(this).parent().data("product-no");
+    var subProductNo = $(this).data("sub-product-no");
+    var subProduct = $(this).data("sub-product");
+    var tagProduct = $(this).parent().prev();
+    var tagInputProduct = $(this).parent().next().find('.product');
+    var tagInputProductNo = $(this).parent().next().find('.product-no');
+    var product = tagProduct.text();
+    var newProduct = product + ':' + subProduct;
+    var newProductNo = productNo + '-' + subProductNo;
+    tagInputProduct.val(newProduct);
+    tagInputProductNo.val(newProductNo)
+  });
+
+
+
 });
